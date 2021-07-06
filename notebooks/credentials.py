11da7b69b451973d6383dbd2abeaa1c35adaca34
Lucas Raniére <lucas.raniere94@gmail.com>
2021-07-06 11:27:30 -0300

    adding the automatic week tweets retrieval

---
+++ notebooks/credentials.py
@@ -0,0 +1,10 @@
+'''
+Comsumer keys and access tokens to Twitter API authentication
+'''
+# consumer keys
+CONSUMER_KEY = '7CURIhLRSeNUv5HUT3gRxk0W9'
+CONSUMER_SECRET = 'AAivD3UIQ0qwFNFMm0vuBcZlfCiEO5moUYzPGrsOjERmWBVvaf'
+
+# access tokens
+ACCESS_TOKEN = '904788399789408257-Mb5wqSxIyTvMhfOWYJ2IsBWq4vY60Mh'
+ACCESS_SECRET = 'vuM0wKtibAOoTyV0qWOMjSeouM49Rb4tE5F2O6nZc0ghw'
