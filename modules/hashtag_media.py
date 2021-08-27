997d081289754d0ede1c97b017fe183d418c4be4
Lucas Raniére <lucas.raniere94@gmail.com>
2021-08-27 09:29:22 -0300

    just updating to sync the OSs

--- modules/hashtag_media.py
+++ modules/hashtag_media.py
@@ -5,4 +5,4 @@ hashtag_media = ['g1', 'uolentrevista', 'globonews', 'uolnews', 'portalig', 'new
                 'novodia', 'pnbonline', 'cnn', 'rádio', 'jornaldagazeta', 'faroldabahia', 'factchecking', 'blogtuliolemos', 'conexãoglobonews',
                 'ariquemesonline', 'tuneinbrasil', 'bnews', 'notícias', 'opopular', 'JornalDaRecord', 'JR24H', 'GloboNews', 'Edicao16', 'oFuxiqueiro',
                 'JornalNacional', 'jornalnacional', 'JN', 'EmPauta', 'G1', 'JG', 'Jn', 'Bandnewsfm', 'JornaldaRecord', 'CnnBrasil', 'UOLDebate', 'UOLEntrevista',
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101']
+                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico', 'Aprizion', 'Fantastico', 'OpiniaoNoAr']
