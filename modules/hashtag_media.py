ec109b2cafb258986ba0dbb29e42896d0a82989b
Lucas Raniére <lucas.raniere94@gmail.com>
2021-09-01 19:39:03 -0300

    bla bla

--- modules/hashtag_media.py
+++
@@ -1,8 +0,0 @@
-hashtag_media = ['g1', 'uolentrevista', 'globonews', 'uolnews', 'portalig', 'news', 'bot', 'panfleterobot', 'jornaldacultura', 'jn',
-                'aprizion', 'breakingnews', 'breaking_news', 'odia', 'noarnacbn', 'canaisglobo', 'aovivo', 'urgente', 'política',
-                'politica', 'bandjornalismo', 'notíciaatual', 'rodaviva', 'uol', 'msnbrasil', 'nexoexpresso', 'r7', 'noticias', 'conexaoglobonews',
-                'estudioi', 'meme', 'exclusivo', 'informação', 'jornalismo', 'edicao18', 'notícia', '15minutosgazetadopovo', 'rádiopampa', 'jornaldarecord',
-                'novodia', 'pnbonline', 'cnn', 'rádio', 'jornaldagazeta', 'faroldabahia', 'factchecking', 'blogtuliolemos', 'conexãoglobonews',
-                'ariquemesonline', 'tuneinbrasil', 'bnews', 'notícias', 'opopular', 'JornalDaRecord', 'JR24H', 'GloboNews', 'Edicao16', 'oFuxiqueiro',
-                'JornalNacional', 'jornalnacional', 'JN', 'EmPauta', 'G1', 'JG', 'Jn', 'Bandnewsfm', 'JornaldaRecord', 'CnnBrasil', 'UOLDebate', 'UOLEntrevista',
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico', 'Aprizion', 'Fantastico', 'OpiniaoNoAr']
