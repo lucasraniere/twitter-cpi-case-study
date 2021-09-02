a0c60b9a119ae5b406d964562ecf44be1a10f6f9
Lucas Raniére <lucas.raniere94@gmail.com>
2021-09-01 21:56:52 -0300

    re-updating repo

--- modules/hashtag_media.py
+++ modules/hashtag_media.py
@@ -1,26 +1,22 @@
-hashtag_media = ['g1', 'uolentrevista', 'globonews', 'uolnews', 'portalig', 'news', 'bot', 'panfleterobot', 'jornaldacultura', 'jn',
-                'aprizion', 'breakingnews', 'breaking_news', 'odia', 'noarnacbn', 'canaisglobo', 'aovivo', 'urgente', 'política',
-                'politica', 'bandjornalismo', 'notíciaatual', 'rodaviva', 'uol', 'msnbrasil', 'nexoexpresso', 'r7', 'noticias', 'conexaoglobonews',
-                'estudioi', 'meme', 'exclusivo', 'informação', 'jornalismo', 'edicao18', 'notícia', '15minutosgazetadopovo', 'rádiopampa', 'jornaldarecord',
-                'novodia', 'pnbonline', 'cnn', 'rádio', 'jornaldagazeta', 'faroldabahia', 'factchecking', 'blogtuliolemos', 'conexãoglobonews',
-                'ariquemesonline', 'tuneinbrasil', 'bnews', 'notícias', 'opopular', 'JornalDaRecord', 'JR24H', 'GloboNews', 'Edicao16', 'oFuxiqueiro',
-                'JornalNacional', 'jornalnacional', 'JN', 'EmPauta', 'G1', 'JG', 'Jn', 'Bandnewsfm', 'JornaldaRecord', 'CnnBrasil', 'UOLDebate', 'UOLEntrevista',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico', 'Aprizion', 'Fantastico', 'OpiniaoNoAr', 'Edicao18',
-                'RedePampa', 'jornalpiracicabano', 'braincast', 'BREAKING', 'ManhãBandeirantes', 'CopaAmericaNoSBT', 'GNewsEmPonto', 'OlimpiadasNaGlobo', 'FatosDaSemana',
-                'Coluna', 'shorts', 'PrimeiroCafe', 'TênisNaESPN', 'DF1', 'J10', 'PapodePolítica', 'ConversaComBiaEMau', 'podcast', 'infojurid', 'conjur', 'RedeTVT',
-                'ficaadica']
-=======
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101']
->>>>>>> parent of 997d081 (just updating to sync the OSs)
-=======
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico', 'Aprizion', 'Fantastico', 'OpiniaoNoAr']
->>>>>>> parent of 1ffadba (updating repository)
-=======
-                'RodaViva', 'UOLNews', '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico', 'Aprizion', 'Fantastico', 'OpiniaoNoAr', 'Edicao18',
-                'RedePampa', 'jornalpiracicabano', 'braincast', 'BREAKING', 'ManhãBandeirantes', 'CopaAmericaNoSBT', 'GNewsEmPonto', 'OlimpiadasNaGlobo', 'FatosDaSemana',
-                'Coluna', 'shorts', 'PrimeiroCafe', 'TênisNaESPN', 'DF1', 'J10', 'PapodePolítica', 'ConversaComBiaEMau', 'podcast', 'infojurid', 'conjur', 'RedeTVT',
-                'ficaadica']
->>>>>>> parent of 7d1140e (updating repository)
+hashtag_media = ['g1', 'uolentrevista', 'globonews', 'uolnews', 'portalig', 'news',
+       'bot', 'panfleterobot', 'jornaldacultura', 'jn', 'aprizion',
+       'breakingnews', 'breaking_news', 'odia', 'noarnacbn',
+       'canaisglobo', 'aovivo', 'urgente', 'política', 'politica',
+       'bandjornalismo', 'notíciaatual', 'rodaviva', 'uol', 'msnbrasil',
+       'nexoexpresso', 'r7', 'noticias', 'conexaoglobonews', 'estudioi',
+       'meme', 'exclusivo', 'informação', 'jornalismo', 'edicao18',
+       'notícia', '15minutosgazetadopovo', 'rádiopampa', 'jornaldarecord',
+       'novodia', 'pnbonline', 'cnn', 'rádio', 'jornaldagazeta',
+       'faroldabahia', 'factchecking', 'blogtuliolemos',
+       'conexãoglobonews', 'ariquemesonline', 'tuneinbrasil', 'bnews',
+       'notícias', 'opopular', 'JornalDaRecord', 'JR24H', 'GloboNews',
+       'Edicao16', 'oFuxiqueiro', 'JornalNacional', 'jornalnacional',
+       'JN', 'EmPauta', 'G1', 'JG', 'Jn', 'Bandnewsfm', 'JornaldaRecord',
+       'CnnBrasil', 'UOLDebate', 'UOLEntrevista', 'RodaViva', 'UOLNews',
+       '3em1', 'BBB21', 'CapaDoDia', 'BBB101', 'Fantástico', 'fantastico',
+       'Aprizion', 'Fantastico', 'OpiniaoNoAr', 'Edicao18', 'RedePampa',
+       'jornalpiracicabano', 'braincast', 'BREAKING', 'ManhãBandeirantes',
+       'CopaAmericaNoSBT', 'GNewsEmPonto', 'OlimpiadasNaGlobo',
+       'FatosDaSemana', 'Coluna', 'shorts', 'PrimeiroCafe', 'TênisNaESPN',
+       'DF1', 'J10', 'PapodePolítica', 'ConversaComBiaEMau', 'podcast',
+       'infojurid', 'conjur', 'RedeTVT', 'ficaadica']
