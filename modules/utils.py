aad9ff239e0f49867c0b4d3bd412f55a7e5c18f0
Lucas Raniere <lucas.raniere94@gmail.com>
2022-03-28 09:34:04 -0300

    back up before refactoring

--- modules/utils.py
+++ modules/utils.py
@@ -4,5 +4,16 @@ def hello_world():
     print('Hello World')
 
 
-def load_week_list():
-    pass
+def load_week_list(data_path):
+    DATA_PATH = data_path
+    week_list = [week_dir for week_dir in os.listdir(DATA_PATH) if os.path.isdir(DATA_PATH+week_dir) and not week_dir.endswith('.ipynb_checkpoints')]
+    week_list.sort()
+    week_list.remove('week_pr_01')
+    week_list.insert(week_list.index('week_12')+1, 'week_pr_01')
+    week_list.remove('week_pr_02')
+    week_list.insert(week_list.index('week_pr_01')+1, 'week_pr_02')
+    week_list.remove('week_pr_03')
+    week_list.insert(week_list.index('week_17')+1, 'week_pr_03')
+    week_list.remove('week_pr_04')
+    week_list.insert(week_list.index('week_21')+1, 'week_pr_04')
+    return week_list
